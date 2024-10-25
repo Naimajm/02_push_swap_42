0c07a18d1ebbfd55b8b7d2bfcd61c6c53581050e
Juan Manuel Gomez Martin <juagomez@c3r2s4.42malaga.com>
2024-10-25 13:51:53 +0200

    v0.1 falla la impresion comandos push_swap

--- bonus/checker.c
+++ bonus/checker.c
@@ -6,7 +6,181 @@
 /*   By: juagomez <juagomez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 13:23:03 by juagomez          #+#    #+#             */
-/*   Updated: 2024/10/24 13:23:04 by juagomez         ###   ########.fr       */
+/*   Updated: 2024/10/25 13:49:57 by juagomez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "checker.h"
+
+static int	ft_strcmp_checker(char *str_1, char *str_2)
+{
+	while (*str_1 == *str_2
+		&& *str_1)
+	{
+		++str_1;
+		++str_2;
+	}
+	return (*str_1 - *str_2);
+}
+
+//#include "../push_swap/push_swap.h"
+
+/** 
+* @brief Recibe strings comandos del ordenamiento del stack, impresos a traves 
+del fd 1 (STDIN_FILENO). Lee cada linea de comandos y replica el ordenamiento
+realizado.  
+* @param stack_a: puntero a la lista vinculada stack a.
+* @param stack_b: puntero a la lista vinculada stack b.
+* @param command: string recibido de la lectura de la linea en STDIN_FILENO.
+* @returns void -> Lanza error si hay string diferente alguno de los comandos.
+*/
+static void	check_command(t_stack **stack_a, t_stack **stack_b, char *command)
+{
+	if (ft_strcmp_checker(command, "pa\n") == 0)
+	{
+		pa(stack_a, stack_b, true);
+		//printf("parse_command() -> pa \n");
+	}
+	else if (!ft_strcmp_checker(command, "pb\n"))
+	{
+		pb(stack_b, stack_a, true);
+		//printf("parse_command() -> pb \n");
+	}
+	else if (ft_strcmp_checker(command, "ra\n") == 0)
+	{
+		ra(stack_a, true);		
+		//printf("parse_command() -> ra \n");
+	}
+	else if (!ft_strcmp_checker(command, "rb\n"))
+		rb(stack_b, true);
+	else if (ft_strcmp_checker(command, "rr\n") == 0)
+		rr(stack_a, stack_b, true);
+	else if (ft_strcmp_checker(command, "rra\n") == 0)
+		rra(stack_a, true);
+	else if (ft_strcmp_checker(command, "rrb\n") == 0)
+		rrb(stack_b, true);
+	else if (!ft_strcmp_checker(command, "rrr\n"))
+		rrr(stack_a, stack_b, true);
+	else if (ft_strcmp_checker(command, "sa\n") == 0)
+		sa(stack_a, true);
+	else if (ft_strcmp_checker(command, "sb\n") == 0)
+		sb(stack_b, true);
+	else
+	{
+		ft_free_stack(stack_a);
+		ft_free_stack(stack_b);
+		ft_putstr_fd("Error\n", 2);
+		exit(1);
+	}	
+}
+
+/* static void	parse_command(t_stack **stack_a, t_stack **stack_b, char *command)
+{
+	if (ft_strncmp(command, "pa\n", 3) == 0)
+	{
+		pa(stack_a, stack_b, true);
+		printf("parse_command() -> pa \n");
+	}
+	else if (ft_strncmp(command, "pb\n", 3) == 0)
+	{
+		pb(stack_b, stack_a, true);
+		printf("parse_command() -> pb \n");
+	}
+	else if (ft_strncmp(command, "ra\n", ft_strlen("ra\n")) == 0)
+	{
+		ra(stack_a, true);		
+		printf("parse_command() -> ra \n");
+	}
+	else if (ft_strncmp(command, "rb\n", ft_strlen("rb\n")) == 0)
+		rb(stack_b, true);
+	else if (ft_strncmp(command, "rr\n", ft_strlen("rr\n")) == 0)
+		rr(stack_a, stack_b, true);
+	else if (ft_strncmp(command, "rra\n", ft_strlen("rra\n")) == 0)
+		rra(stack_a, true);
+	else if (ft_strncmp(command, "rrb\n", ft_strlen("rrb\n")) == 0)
+		rrb(stack_b, true);
+	else if (ft_strncmp(command, "rrr\n", ft_strlen("rrr\n")) == 0)
+		rrr(stack_a, stack_b, true);
+	else if (ft_strncmp(command, "sa\n", ft_strlen("sa\n")) == 0)
+		sa(stack_a, true);
+	else if (ft_strncmp(command, "sb\n", ft_strlen("sb\n")) == 0)
+		sb(stack_b, true);
+	else
+	{
+		ft_free_stack(stack_a);
+		ft_free_stack(stack_b);
+		ft_putstr_fd("Error\n", 2);
+		exit(1);
+	}	
+} */
+
+int	main(int argc, char **argv)
+{
+	// DECLARACION + INICIALIZACION LISTAS VINCULADAS 'STACKS
+	t_stack	*stack_a;
+	t_stack	*stack_b;
+	char	*next_line;
+	int		stack_len;
+
+	stack_a = NULL;
+	stack_b = NULL;
+
+	// RECEPCION + PROCESAMIENTO ARGUMENTOS
+	if (argc == 1)
+		return (0);
+	else if (argc == 2)
+		argv = split(argv[1],' ');
+	
+	// CREACION STACK
+	stack_init(&stack_a, argv + 1);
+
+	// IMPRESION STACK 'INICIAL'
+	/* t_stack *stack_copy_a;
+	stack_copy_a = stack_a;
+	while (stack_copy_a) 
+	{
+		printf( "main() stack a -> arg numero-> %d, \t prev-> %p \t address-> %p \t next-> %p \n", stack_copy_a->value, stack_copy_a->prev, stack_copy_a, stack_copy_a->next );
+		stack_copy_a = stack_copy_a->next;
+	} */
+
+	stack_len = ft_stack_len(stack_a);
+	
+	//next_line = NULL;	
+	next_line = get_next_line_check(1);
+
+	//while (next_line)
+	while (next_line)
+	{
+		// LEER COMANDOS IMPRIMIDOS POR STDIN
+		//next_line = get_next_line(STDIN_FILENO);
+		//next_line = get_next_line(1);
+		//printf("%i \n", ft_strncmp((const char *)(next_line), "pa\n", ft_strlen("pa\n")));
+		//printf("next_line -> %s", next_line);
+		check_command(&stack_a, &stack_b, next_line);
+		next_line = get_next_line_check(1);
+		//next_line = get_next_line(STDIN_FILENO);
+
+	}
+	
+	// VALIDACION ORDENAMIENTO STACK + IGUAL NUM ELEMENTOS en estados inicial y final
+	if ((stack_is_sorted(stack_a)) && (ft_stack_len(stack_a) == stack_len) && ft_stack_len(stack_a) > 0)
+		ft_putstr_fd("OK\n", 1);
+	else
+		ft_putstr_fd("KO\n", 1);	
+
+	// IMPRESION STACK 'FINAL'
+	/* stack_copy_a = stack_a;
+	while (stack_copy_a) 
+	{
+		printf( "main() stack a -> arg numero-> %d, \t prev-> %p \t address-> %p \t next-> %p \n", stack_copy_a->value, stack_copy_a->prev, stack_copy_a, stack_copy_a->next );
+		stack_copy_a = stack_copy_a->next;
+	} */
+
+	// LIBERAR MEMORIA STACK
+	ft_free_stack(&stack_a);
+	return (0);
+}
+
+
+
 
--- bonus/checker.h
+++ bonus/checker.h
@@ -6,7 +6,22 @@
 /*   By: juagomez <juagomez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 13:34:41 by juagomez          #+#    #+#             */
-/*   Updated: 2024/10/24 13:34:43 by juagomez         ###   ########.fr       */
+/*   Updated: 2024/10/25 13:48:12 by juagomez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef CHECKER_H
+# define CHECKER_H
+
+/* OWN LIBRARIES */
+
+#include "../push_swap/push_swap.h"
+
+#include "../get_next_line/get_next_line.h"
+
+# define BUFFER_SIZE 10
+
+char	*get_next_line_check(int fd);
+
+
+#endif
