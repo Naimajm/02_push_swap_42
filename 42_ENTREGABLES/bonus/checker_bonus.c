0eb76e2fafa8ec93a402d63aa4adc1f117b902e6
Juanma Gomez <juanma.gomez.martin@gmail.com>
2024-11-18 20:04:28 +0100

    v2.2 checker  leaks error comandos ok

--- 42_ENTREGABLES/bonus/checker_bonus.c
+++ 42_ENTREGABLES/bonus/checker_bonus.c
@@ -6,15 +6,15 @@
 /*   By: juagomez <juagomez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 13:23:03 by juagomez          #+#    #+#             */
-/*   Updated: 2024/11/18 09:30:33 by juagomez         ###   ########.fr       */
+/*   Updated: 2024/11/18 20:03:19 by juagomez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "checker_bonus.h"
 
-static void	process_commands(t_stack **stack_a, t_stack **stack_b);
-static void	check_command(t_stack **stack_a, t_stack **stack_b, char *command);
-static void	free_all_stack(t_stack **stack_a, t_stack **stack_b);
+static char	**parse_arguments_checker(int argc, char **argv);
+static int	process_commands(t_stack **stack_a, t_stack **stack_b);
+static int	check_command(t_stack **stack_a, t_stack **stack_b, char *command);
 static void	free_split_argvs(char **split_args);
 
 int	main(int argc, char **argv)
@@ -22,22 +22,18 @@ int	main(int argc, char **argv)
 	t_stack	*stack_a;
 	t_stack	*stack_b;
 	char	**split_args;
+	int		error_flag;
 
 	stack_a = NULL;
 	stack_b = NULL;
-	split_args = NULL;
-	if (argc == 1)
-		return (0);
-	else if (argc == 2 && !(argv[1][0] == 0))
-	{
-		split_args = split_push_swap(argv[1], ' ');
+	split_args = parse_arguments_checker(argc, argv);
+	if (split_args)
 		argv = split_args;
-	}
 	init_stack(&stack_a, argv + 1);
-	process_commands(&stack_a, &stack_b);
+	error_flag = process_commands(&stack_a, &stack_b);
 	if ((is_stack_sorted(stack_a)) && get_stack_len(stack_a) > 0)
 		ft_putstr_fd("OK\n", 1);
-	else
+	else if (!is_stack_sorted(stack_a) && !error_flag)
 		ft_putstr_fd("KO\n", 1);
 	free_stack(&stack_a);
 	free_stack(&stack_b);
@@ -46,21 +42,43 @@ int	main(int argc, char **argv)
 	return (0);
 }
 
-static void	process_commands(t_stack **stack_a, t_stack **stack_b)
+static char	**parse_arguments_checker(int argc, char **argv)
+{
+	char	**split_args;
+
+	if (argc == 1)
+		return (0);
+	else if (argc == 2 && !(argv[1][0] == 0))
+	{
+		split_args = split_push_swap(argv[1], ' ');
+		return (split_args);
+	}
+    return (NULL);
+}
+
+static int	process_commands(t_stack **stack_a, t_stack **stack_b)
 {
 	char	*next_line;
+	int 	error_command;
 
+	error_command = 0;
 	next_line = get_next_line(STDIN_FILENO);
 	while (next_line)
 	{
-		check_command(stack_a, stack_b, next_line);
+		error_command = check_command(stack_a, stack_b, next_line);
+		if (error_command)
+			ft_putstr_fd("Error\n", 2);
 		free(next_line);
 		next_line = get_next_line(STDIN_FILENO);
 	}
+	free(next_line);
+	return (error_command);
 }
 
-static void	check_command(t_stack **stack_a, t_stack **stack_b, char *command)
+static int	check_command(t_stack **stack_a, t_stack **stack_b, char *command)
 {
+	int	error_command;
+	
 	if (ft_strncmp(command, "pa\n", ft_strlen("pa\n")) == 0)
 		pa(stack_a, stack_b, false);
 	else if (ft_strncmp(command, "pb\n", ft_strlen("pb\n")) == 0)
@@ -82,17 +100,8 @@ static void	check_command(t_stack **stack_a, t_stack **stack_b, char *command)
 	else if (ft_strncmp(command, "sb\n", ft_strlen("sb\n")) == 0)
 		sb(stack_b, false);
 	else
-		free_all_stack(stack_a, stack_b);
-}
-
-static void	free_all_stack(t_stack **stack_a, t_stack **stack_b)
-{
-	if (!stack_a)
-		free_stack(stack_a);
-	if (!stack_b)
-		free_stack(stack_b);
-	ft_putstr_fd("Error\n", 2);
-	exit(1);
+		return (error_command = 1);
+	return (error_command = 0);
 }
 
 static void	free_split_argvs(char **split_args)
